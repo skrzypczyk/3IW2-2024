7e8337e4c43cf485a4512cfc909c9f1f51067f57
Yves Skrzypczyk <ysk@wizards-technologies.com>
2024-10-22 10:58:26 +0200

    TP routing

--- www/Public/index.php
+++ www/Public/index.php
@@ -1,1 +1,17 @@
-Bonjour
+<?php
+
+/*
+ *  TP de routing
+ *  - Récupérer l'url exemple : http://localhost/user/Add
+ *  - Récupérer plus précisement l'URI : /user/Add
+ *  - Nettoyer l'URI : exemple minuscules ....
+ *  - Faire une relation entre /user/add et controller User et l'acion register
+ *      --> Récupérer sous forme de tableau le contenu de routes.yml
+ *      --> Récupérer le controller et l'action associé à l'URI
+ *  - Appeler de manière dynamique le bon controller et la bonne action, exemple :
+ *      --> $controller = new User();
+ *      --> $controller->register();
+ *
+ * (Toujours garder en tête de faire un maximum de vérification avec des affichages d'erreur)
+ */
+
